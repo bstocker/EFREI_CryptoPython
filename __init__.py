0af8c8efc7f503aa6925ac0fa8e5bbca9b71dbda
Nikaaaaaaaa <sebastien.lomellini1@gmail.com>
2025-01-21 14:11:27 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('chenille.html')
+    return render_template('De_6_Faces')
 
 key = Fernet.generate_key()
 f = Fernet(key)
